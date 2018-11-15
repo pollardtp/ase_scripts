eda8982011083b135d5ddee83db55e6030ec0893
Travis Patrick Pollard <teep@centennial03.ib0.arlu.arl.hpc.mil>
2018-11-15 03:24:18 -0500

    Added code for converting cif to gen files

---
+++ general/cif2gen.py
@@ -0,0 +1,9 @@
+#!/p/home/teep/.local_programs/conda3/bin/python
+
+import ase, sys
+from ase import Atom, Atoms
+import ase.io
+
+coords = ase.io.read(sys.argv[1], format='cif')
+genout = ase.io.write(sys.argv[2], coords, format='gen')
+
