eba8cb91a82570f4c09cac2607127807282de9d3
Travis Patrick Pollard <teep@excalibur09.arl.hpc.mil>
2019-07-05 14:55:16 -0400

    functional correction

--- gaussian16/xyz2g16.py
+++ gaussian16/xyz2g16.py
@@ -12,7 +12,7 @@ for file_ in glob.glob('%s/*.xyz' % dir_):
     inp_ = ase.io.read('%s%s' % (filename_, fileext_) , format='xyz')
 
     calc_ = Gaussian(mem='64GB', nproc='32', opt='tight, MaxCycles=360, recalcfc=20', scrf='cpcm, solvent=generic, read', 
-                     scf='tight', integral='ultrafine', freq='analytic', method='UM05-2X', basis='6-31+G(d,p)', symmetry='none',
+                     scf='tight', integral='ultrafine', freq='analytic', method='UM052X', basis='6-31+G(d,p)', symmetry='none',
                      addsec="Eps=8\nEpsInf=1.96")
 
     inp_.set_calculator(calc_)
