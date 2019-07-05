0d29701bffa87f413fe35eaad9be7d0b4c689294
Travis Patrick Pollard <teep@excalibur09.arl.hpc.mil>
2019-07-05 19:44:40 -0400

    Fixed issue with magnetic moments.

--- gaussian16/xyz2g16.py
+++ gaussian16/xyz2g16.py
@@ -11,9 +11,19 @@ for file_ in glob.glob('%s/*.xyz' % dir_):
     filename_, fileext_ = os.path.splitext(file_) # fileext_ carries the .
     inp_ = ase.io.read('%s%s' % (filename_, fileext_) , format='xyz')
 
-    calc_ = Gaussian(mem='64GB', nproc='32', opt='tight, MaxCycles=360, recalcfc=20', scrf='cpcm, solvent=generic, read', 
-                     scf='tight', integral='ultrafine', freq='analytic', method='UM052X', basis='6-31+G(d,p)', symmetry='none',
-                     addsec="Eps=8\nEpsInf=1.96")
+    magmom = []
+
+    for atom in inp_:
+        if ( atom.symbol == 'O' ):
+            magmom.append(0.5)
+        else:
+            magmom.append(0)
+
+    inp_.set_initial_magnetic_moments(magmom)
+
+    calc_ = Gaussian(mem='64GB', nproc='32', opt='tight, MaxCycles=360', scrf='cpcm, solvent=generic, read', 
+                     scf='tight', integral='ultrafine', freq='analytic', method='UPBEPBE', basis='6-31+G(d)', 
+                     symmetry='none', addextra='empiricaldispersion=g3bj', addsec="Eps=8\nEpsInf=1.96")
 
     inp_.set_calculator(calc_)
     calc_.write_input(inp_)
