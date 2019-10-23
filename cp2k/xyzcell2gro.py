5d25cfb67c8e3b34ad116b12f436b1c9dc3103b3
Travis P Pollard <pollartpuc@gmail.com>
2019-10-23 20:23:02 +0000

    Changed format bc cell is 9 params, no angles

--- cp2k/xyzcell2gro.py
+++ cp2k/xyzcell2gro.py
@@ -21,15 +21,17 @@ from ase.io import read, write
 dir = os.getcwd()
 
 # trajectory is XMOL format
-# cell       is step time a b c alpha beta gamma
+# cell       is step time ax ay az bx by bz cx cy cz vol
 for file in glob.glob('%s/*-pos-*.xyz' % dir):
    filename, fileext = os.path.splitext(file)
    fileroot, filescrap = filename.split('-pos')
    trj = read('%s%s' % (filename, fileext) , format='xyz', index=':')
-   cel = np.loadtxt('%s-1.cell' % (fileroot), usecols=range(2,8), dtype=np.double)
+   cel = np.loadtxt('%s-1.cell' % (fileroot), usecols=range(2,11), dtype=np.double)
    nframes = len(trj)
    for frame in range(nframes):
-      trj[frame].set_cell([ cel[frame][0], cel[frame][1], cel[frame][2], cel[frame][3], cel[frame][4], cel[frame][5] ])
+      trj[frame].set_cell([(cel[frame][0], cel[frame][1], cel[frame][2]), 
+                           (cel[frame][3], cel[frame][4], cel[frame][5]), 
+                           (cel[frame][6], cel[frame][7], cel[frame][8])])
       trj[frame].set_pbc([True, True, True])
       write('%s.g96' % (fileroot), trj[frame], format='gromos', append=True)
 
