997938c0af6fdba3aaaa9ec7942d2e7ae26934f8
Travis P. Pollard <pollartpuc@gmail.com>
2021-03-25 16:49:11 -0400

    Updated G16 script

--- gaussian16/xyz2g16.py
+++ gaussian16/xyz2g16.py
@@ -23,21 +23,16 @@ for file in glob.glob('%s/*.xyz' % dir):
     inp = read('%s%s' % (filename, fileext) , format='xyz')
     filter_filename = re.sub(r'[.]','_', filename) # HPC script cuts filename at first .
 
-    magmom = []
+    t1 = re.compile(".*_(q-?(?:\d+,?)+m-?(?:\d+,?)+).*") # regex for charge and spin part of filename
+    t2 = t1.match("%s" % (filter_filename)).group(1) # grabs regex
+    chg = re.split('[a-z]', t2)[1] # is charge
+    spn = re.split('[a-z]', t2)[2] # is spin
 
-    for atom in inp:
-        if ( atom.symbol == 'O' ):
-            magmom.append(0.5)
-        else:
-            magmom.append(0)
+    gsolv = 'pcm, solvent=acetone'
 
-    inp.set_initial_magnetic_moments(magmom)
-
-    calc = Gaussian(mem='64GB', nproc='40', opt='tight, MaxCycles=360', scrf='cpcm, solvent=generic, read',
-                     scf='tight', integral='superfine', freq='analytic', method='UPBEPBE', basis='6-31+G(d)',
-                     symmetry='none', extra='empiricaldispersion=gd3bj', addsec="Eps=8\nEpsInf=1.96")
+    calc = Gaussian(mem='64GB', cpu='40', opt='tight, MaxCycles=360', scrf=gsolv, scf='tight', integral='ultrafine', freq='analytic', method='M052X', basis='6-31+G(d,p)', symmetry='none', charge=int(chg), mult=int(spn))
 
     inp.set_calculator(calc)
     calc.write_input(inp)
-    os.system("mv g16.com %s.gjf" % (filter_filename))
-    os.system("rm g16.ase")
+    os.system("mv Gaussian.com %s.inp" % (filter_filename))
+
